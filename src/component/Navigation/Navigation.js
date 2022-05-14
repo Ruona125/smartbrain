f7315a5e562781451c5ac4da6efae48a7dad479c
ruona <ogheneruonaagadagba4@gmail.com>
2022-05-14 17:05:32 +0100

    installed reactstrap so i can add profile icon and drop-down

--- src/component/Navigation/Navigation.js
+++ src/component/Navigation/Navigation.js
@@ -1,29 +1,29 @@
-import React from 'react';
+import React from "react";
+import ProfileIcon from "../profile/profileIcon";
+
 const Navigation = ({ onRouteChange, isSignedIn }) => {
   if (isSignedIn) {
     return (
-      <nav style={{ display: 'flex', justifyContent: 'flex-end' }}>
+      <nav style={{ display: "flex", justifyContent: "flex-end" }}>
+        <ProfileIcon />
         <p
-          onClick={() => onRouteChange('signout')}
-          className="f3 link dim black underline pa3 pointer"
-        >
+          onClick={() => onRouteChange("signout")}
+          className="f3 link dim black underline pa3 pointer">
           Sign Out
         </p>
       </nav>
     );
   } else {
     return (
-      <nav style={{ display: 'flex', justifyContent: 'flex-end' }}>
+      <nav style={{ display: "flex", justifyContent: "flex-end" }}>
         <p
-          onClick={() => onRouteChange('signin')}
-          className="f3 link dim black underline pa3 pointer"
-        >
+          onClick={() => onRouteChange("signin")}
+          className="f3 link dim black underline pa3 pointer">
           Sign In
         </p>
         <p
-          onClick={() => onRouteChange('register')}
-          className="f3 link dim black underline pa3 pointer"
-        >
+          onClick={() => onRouteChange("register")}
+          className="f3 link dim black underline pa3 pointer">
           Register
         </p>
       </nav>
